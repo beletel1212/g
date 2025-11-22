3d9e896e07278bb85bdf02870743c4b50426ce72
beletel1212 <beletetadese489@gmail.com>
2025-11-21 19:56:51 -0800

    Delete lib directory

--- lib/dialer/repository/call_log_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:call_log/call_log.dart';
-
-class SystemCallLogRepository {
-  /// Fetch REAL Android system call logs
-  Future<List<CallLogEntry>> getSystemCallLogs() async {
-    final Iterable<CallLogEntry> entries = await CallLog.query();
-    return entries.toList();
-  }
-}
